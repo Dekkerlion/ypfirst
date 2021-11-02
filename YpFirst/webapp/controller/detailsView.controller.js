0b724f1ab1574b680a7120fc689510217490d37d
Dekkerlion <32671976+Dekkerlion@users.noreply.github.com>
2021-11-02 14:04:51 +0200

    second view table
    detailsView:
    -added table
    -added buttons

--- YpFirst/webapp/controller/detailsView.controller.js
+++ YpFirst/webapp/controller/detailsView.controller.js
@@ -4,9 +4,16 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("YpFirst.controller.detailsView", {
+		getGlobal: function(property_name) {
+			return this.settingsModel.getProperty("/" + property_name);
+		},
+
+		setGlobal: function(property_name, newValue) {
+			return this.settingsModel.setProperty("/" + property_name, newValue);
+		},
 		onInit: function() {
-			
+			this.oModel = this.getOwnerComponent().getModel();
 		}
-		
+
 	});
 });
