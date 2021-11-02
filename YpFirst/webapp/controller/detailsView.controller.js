909f93c381c090016f3ee0e683cee328f9fbc462
Codrin99 <48892395+Codrin99@users.noreply.github.com>
2021-11-02 17:48:02 +0200

    Read and create with dialog
    New file fragment for  dialog for the create button and read and create operation done working
    
    Need to do the autorefresh after create

--- YpFirst/webapp/controller/detailsView.controller.js
+++ YpFirst/webapp/controller/detailsView.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,MessageToast, Fragment, JSONModel) {
 	"use strict";
 
 	return Controller.extend("YpFirst.controller.detailsView", {
+
 		getGlobal: function(property_name) {
 			return this.settingsModel.getProperty("/" + property_name);
 		},
@@ -11,9 +15,76 @@ sap.ui.define([
 		setGlobal: function(property_name, newValue) {
 			return this.settingsModel.setProperty("/" + property_name, newValue);
 		},
-		onInit: function() {
-			this.oModel = this.getOwnerComponent().getModel();
-		}
 
+		onInit: function() {
+			this.crudModel = this.getOwnerComponent().getModel();
+			this.onReadPress();
+		},
+
+		onReadPress: function(oEvent) {
+            var _this = this;
+            this.crudModel.read("/StocksSet", {
+                method: "GET",
+                // filters: allFilters,
+                success: function(responseData) {
+                    var readModel = new JSONModel();
+                    readModel.setData(responseData);
+                    _this.getView().setModel(readModel);
+                },
+                error: function(callError) {
+                    alert(callError.message + "\n" + callError.responseText);
+                }
+            });
+            // var _this = this;
+            // var allFilters = [];
+            // var filter1 = new Filter({
+            //  path: "Matnr",
+            //  operator: FilterOperator.EQ,
+            //  value1: "80"
+            // });
+            // allFilters.push(filter1);
+        },
+		
+		 onCreatePress: function(oEvent) {
+			var oEntry = {
+				"Matnr": "",
+				"Location": this.getView().byId("newLocation").getValue(),
+				"Quantity": this.getView().byId("newQuantity").getValue(),
+				"Meins": "PC"
+			};
+
+			this.crudModel.create("/StocksSet", oEntry, {
+				method: "POST",
+				success: function(executionResult) {
+					alert("Created!");
+				},
+				error: function(callError) {
+					alert(callError.message + "\n" + callError.responseText);
+				}
+			});
+		},
+		onOpenDialog : function () {
+			var oView = this.getView();
+
+			// create dialog lazily
+			if (!this.byId("createStock")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "YpFirst.view.createStockDialog",
+					controller: this
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("createStock").open();
+			}
+		},
+
+		onCloseDialog : function () {
+			this.byId("createStock").close();
+		}
 	});
 });
